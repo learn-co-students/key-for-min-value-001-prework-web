e2391a787d45281496b9c026b6ef2aedc508ee37
matthewbillienyc <matthewbillienyc@gmail.com>
2015-09-01 00:03:14 -0400

    passed, but using .sort

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
+
 
 def key_for_min_value(name_hash)
-
+  values = []
+  name_hash.each do |k, v| 
+    values << v
+  end
+  values_ordered = values.sort do |a, b|
+    a <=> b
+  end
+  key_for_min = nil
+  name_hash.each do |k, v|
+    if v == values_ordered[0]
+      key_for_min = k
+    end
+  end
+  key_for_min
 end
