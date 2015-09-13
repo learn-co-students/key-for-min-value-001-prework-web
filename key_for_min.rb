ad477961ce5d6076982e6734fe3804674fd61250
pedrotp <pedro@pedrotp.com>
2015-09-13 01:30:12 -0400

    hads tied but made it happen

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+  control = 10000
+  name_output = ""
+  if name_hash == {}
+    name_output = nil
+  else
+    name_hash.each do |name, value|
+      if value.to_i < control 
+        control = value
+        name_output = name
+      end
+    end
+  end
+  name_output
 end
