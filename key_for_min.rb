831e084bc97f259458b90976b0eff3b9c9d86903
chenghw <ho.won.cheng@gmail.com>
2015-09-22 17:50:44 -0400

    smallest hash complete

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_value_holder = nil
+  min_key_holder = nil
+  name_hash.each do |key, value|
+    if min_value_holder == nil
+      min_value_holder = value
+      min_key_holder = key
+    elsif min_value_holder > value
+      min_value_holder = value
+      min_key_holder = key
+    end
+  end
+  min_key_holder
 end
