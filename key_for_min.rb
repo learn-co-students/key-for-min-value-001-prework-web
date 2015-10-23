3c48176984e5b378789e927c6969b25da7ba824b
flats <dave@robador.com>
2015-10-23 04:21:49 -0400

    first commit - return min val of hash

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_val = nil
+  min_key = nil
+  name_hash.each { |k, v|
+    if min_val == nil
+      min_val = v
+      min_key = k
+    elsif v < min_val
+      min_val = v
+      min_key = k
+    end
+  }
+  min_key
 end
