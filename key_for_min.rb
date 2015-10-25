409a6a91ee38a795405e31ff0f2b3a75594742a2
Beondel <bengmacmillan@gmail.com>
2015-10-24 21:30:52 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,6 @@
 
 def key_for_min_value(name_hash)
     low_key = nil
-    for key, value in name_hash
-        if low_key == nil || value < name_hash[low_key]
-            low_key = key
-        end
-    end
+    name_hash.each_key  {low_key = key if low_key == nil || name_hash[key] < name_hash[low_key]}
     low_key
 end
