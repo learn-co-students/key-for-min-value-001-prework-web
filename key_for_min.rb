3431e872f7e5ed3e91af90e004bcd67096ac30b4
psswf1172 <jennifermabella@gmail.com>
2015-11-21 17:41:26 -0500

    key_for_min done

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    puts "nil"
+  else
+    name_hash.min_by(&:last).first
+  end
 end
+
+# this is a really cheap work around and perhaps a proof with testing that the ends justify the means?
