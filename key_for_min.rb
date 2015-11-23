5167556d5fe6f7295c60109f3ef58a6834c206e8
shmulim <shmuel.markel@gmail.com>
2015-11-22 20:44:50 -0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,7 @@
 def key_for_min_value(name_hash)
   cached_key, cached_value = nil
   name_hash.each do |key, value|
-    if cached_value.nil?
-      cached_key, cached_value = key, value
-    elsif value < cached_value
+    if cached_value.nil? || value < cached_value     
       cached_key, cached_value = key, value
     end
   end
