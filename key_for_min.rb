f02d6897f3ff1f98ae68b05fe3b082f704bd5ad8
burkefielder <burke.fielder@gmail.com>
2015-12-20 20:44:01 -0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  ages = []
+  name_hash.collect do |name, age|
+    ages << age
+  end
+    lowest_age = nil
+    if ages[0] < ages[1]
+      lowest_age = ages[0]
+    elsif ages[1] < ages[2]
+      lowest_age = ages[1]
+    elsif ages[2] < ages[0]
+      lowest_age = ages[2]
+    end
+  name_hash.key(lowest_age)
 end
+
+
+# key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
