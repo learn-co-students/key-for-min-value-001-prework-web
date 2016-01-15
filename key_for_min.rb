91d7ee8d5a8c2e4eca25caf3dab2bcee21f76e19
fossbender <fossbender@gmail.com>
2016-01-15 11:03:56 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+def key_for_min_value(item_hash)
+  count = 0
+  item_array = []
+  num_array = []
 
-def key_for_min_value(name_hash)
+  item_hash.each do |item,num|
+    item_array << item
+    num_array << num
+  end
 
+  low_num = num_array[count]
+  return_item = item_array[count]
+
+  while count < num_array.size
+   if num_array[count] < low_num
+     return_item = item_array[count]
+   end
+     count += 1
+  end
+  return_item
 end
