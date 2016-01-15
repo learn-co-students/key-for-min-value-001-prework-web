2643766dc924ba28bcc66fc622815e607a3f6762
evzaro <evzaro@gmail.com>
2016-01-15 01:43:47 -0500

    Key for Min Value

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+  smallest = nil
+  key_of_smallest = nil
+  name_hash.each do |k, v|
+   if smallest == nil || v < smallest
+    smallest = v
+    key_of_smallest = k
+  end
+
+  end
+  key_of_smallest
 end
