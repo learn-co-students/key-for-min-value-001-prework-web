87bf6060390f73d6b60f4e792be37cbe82290c80
chasejthompson <chase@theprospectgroup.com>
2016-03-08 11:10:31 +0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,9 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
   else
-  result << name_hash.sort_by {|key, value| value}.first
-  result[0][0]
+  result << name_hash.sort_by {|key, value| value}.to_h.keys[0]
+  result[0]
   end
 end
+
+#is it better to call [0][0][0] on result or try to further operate on what I am shovelling to 
