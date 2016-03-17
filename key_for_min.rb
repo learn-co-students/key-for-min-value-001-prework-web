0931b0c5e0c2721b67c3cb8a86faa6ad1237ccfe
alemosie <alexandra.siega@gmail.com>
2016-03-17 13:29:06 -0400

    collect, sort, first, each

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,23 @@
 
 def key_for_min_value(name_hash)
 
+  if name_hash.length > 0
+
+    valueArray = []
+    name_hash.collect do |name, value|
+      valueArray << value
+    end
+    
+    smallestValue = valueArray.sort.first
+      
+    nameKey = ""
+    name_hash.each do |name, value|
+      if smallestValue == value
+          nameKey = name
+      end
+    end
+      
+    nameKey
+    
+  end
 end
