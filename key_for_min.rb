b5211dfa9ca7c4345dedef5225800f58a975da74
abcheromar <abcheromar@gmail.com>
2016-04-05 16:54:14 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,20 @@
 
 def key_for_min_value(name_hash)
 
+   return nil if name_hash.empty?
+   smallest_key, smallest_value = name_hash.first
+   
+   name_hash.each do |key, value| 
+    if value < smallest_value
+      smallest_value = value
+      smallest_key = key
+   end
+  end
+   smallest_key
 end
+
+
+
+  
+
+
