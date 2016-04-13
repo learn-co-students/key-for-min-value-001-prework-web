0440fe311a0198da01da4cde5330648195d27cbc
blessedsoy <unmigong@gmail.com>
2016-04-13 12:23:26 -0400

    Key for Min Value

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,23 @@
 
 def key_for_min_value(name_hash)
 
+  value = [];
+  key = [];
+  name_hash.each do |k, v|
+    if value.length == 0
+      value.push(v)
+      key.push(k)
+    else
+        if value[0] > v
+          value.push(v)
+          value.shift
+          key.push(k)
+          key.shift
+      end
+
+    end
+  end
+    key[0]
 end
+
+key_for_min_value({:blake => 10, :ashley => 50, :adam => 17})
