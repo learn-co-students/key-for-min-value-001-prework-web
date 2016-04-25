266f4a1d5b7df362a983bb4663b5af748155542b
jinzh3ng <jinzheng87@gmail.com>
2016-04-25 17:25:51 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
    if min_number == nil
      min_key = key
      min_number = value 
-   elsif min_number < value
+   elsif min_number > value
      min_key = key
      min_number = value
    end
