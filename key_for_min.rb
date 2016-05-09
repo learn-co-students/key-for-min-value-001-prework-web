4ae0ee10338b4f84439caa09d2f94fc6f72fff40
Ryan Palermo <rpalermodrums@Ryan-Palermos-MacBook-Pro.local>
2016-05-08 20:54:37 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash.empty?
+  names, ages = [], []
+  name_hash.map do |name, age|
+    names << name
+    ages << age
+  end
+  min = ages.min
+  names[ages.index(min)]
 end
