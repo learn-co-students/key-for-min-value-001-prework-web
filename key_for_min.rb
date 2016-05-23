6aedd9b1186fd3495cb3fdddd08d141e0cb1bf33
Nathaniel Miller <dukeoflaser@gmail.com>
2016-05-23 02:00:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,18 @@ def key_for_min_value(name_hash)
   end
 
   selected = name_hash.select do |key, value|
-    value == values.sort[0]
+    value == smallest_value(values)
   end
 
   selected.keys[0]
+end
+
+def smallest_value(values_array)
+  smallest = values_array[0]
+  
+  values_array.each do |v|
+    smallest = v if v < smallest
+  end
+  
+  smallest
 end
