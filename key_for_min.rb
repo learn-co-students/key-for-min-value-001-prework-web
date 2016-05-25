07507db4eef8e3925554530340dba7341059dde3
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-25 13:11:34 -0400

    Done. Cleaned up.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,5 @@
 def key_for_min_value(name_hash)
-	min_value = 1000000
-	name_hash.each { |key, value| min_value = value if value < min_value }
-	name_hash.key(min_value)
+  min_value = 1000000
+  name_hash.each { |key, value| min_value = value if value < min_value }
+  name_hash.key(min_value)
 end 
