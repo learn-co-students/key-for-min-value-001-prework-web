25684be82772ce0c3cd7ac8a65b253eb1844134f
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-25 13:04:15 -0400

    Done. Cleaned up code.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,21 +1,5 @@
 def key_for_min_value(name_hash)
-
-  min_num = 100000000
-  key_for_min_value = 0
- 
-  if name_hash.empty?
-    return nil
-  else
-    name_hash.collect do |key, value|
-      if value < min_num
-        min_num = value
-        key_for_min_value = key
-      else
-        min_num = min_num
-        key_for_min_value = key_for_min_value
-      end
-    end 
-    
-    return key_for_min_value
-  end 
+	min_value = 1000000
+	name_hash.each { |key, value| min_value = value if value < min_value }
+	name_hash.key(min_value)
 end 
