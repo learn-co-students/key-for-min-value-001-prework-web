352cd0a77b9bd89cbff25849a6b3a5e5a6021999
Salome Braganza <salomekbg@gmail.com>
2016-06-06 19:53:16 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -12,6 +12,21 @@ def key_for_min_value(hash)
       lowest_value = value
     end
   end
-  
+
   hash.key(lowest_value)
 end
+
+=begin
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+
+  hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value == value
+      lowest_key == key
+    end
+  end
+  lowest_key
+end
+=end
