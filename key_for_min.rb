18baed22d4fab171e1f99cc5384c65d263ecebcf
gnwankwo <gnwankwo03@gmail.com>
2016-06-17 12:41:49 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,27 @@
 # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# Given a hash with numeric values, return the key for the smallest valu
+    
+    def key_for_min_value(name_hash)
+      value_list=name_hash.collect do |keys, values|
+        values
+      end
+      return name_hash.key(find_min(value_list))
+    end
 
-def key_for_min_value(name_hash)
 
-end
+def find_min(nums)
+    counter=0
+    temp_min=nums[0]
+    while counter< (nums.length)
+      if nums[counter] == temp_min
+        temp_min=nums[counter]
+        
+      elsif nums[counter] > temp_min
+        temp_min=temp_min
+      elsif nums[counter] < temp_min
+        temp_min=nums[counter]
+      end
+      counter+=1
+    end
+    return temp_min
+  end
