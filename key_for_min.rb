4c6c7e054c37a77843ded8504ed78a6b144592f4
Daniel Ness <danieljohnness@yahoo.com>
2016-06-22 23:52:47 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
-def key_for_min_value(name_hash)
-
+new_hash = {}
+def key_for_min_value(new_hash)
+  doggy = []
+  new_hash.collect do |key, value|
+    doggy << value
+  end
+  min_val = doggy.min
+  winner = nil
+  new_hash.each do |key, value|
+    if min_val == value
+    winner = key
+    end
+  end
+  winner
 end
