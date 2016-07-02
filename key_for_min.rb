1a029acf6d619c47206e7f89cf1fbdaf66a8b399
Satu Barnhill <satu.barnhill@gmail.com>
2016-07-02 01:58:47 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,21 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  answer = ""
+  if name_hash == {}
+    answer = nil
+  else
+    numbers = name_hash.collect {|name, number| number}
+    names = name_hash.collect {|name, number| name}
+    supposed_index = 0
+    supposed_min_value = numbers[supposed_index]
+    numbers.each_with_index do |number, index|
+      if number < supposed_min_value
+        supposed_index = index
+      else
+      end
+    end
+    answer = names[supposed_index]
+  end
+  answer
 end
