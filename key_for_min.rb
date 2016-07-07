2deb4f58ba8050190afed6c2c82779fc5f3ca45c
mo6709 <mo6709@gmail.com>
2016-07-07 18:26:13 -0400

    much faster

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,21 +3,30 @@
 require 'pry'
 def key_for_min_value(name_hash)
     
-	values = name_hash.collect{|key, value|
-       value
-	}
+	# values = name_hash.collect{|key, value|
+ #       value
+	# }
+	# min_value = values.min
+	# return_variable = nil
+ #      name_hash.each{|key, value|
 
-	min_value = values.min
-	return_variable = nil
-      name_hash.each{|key, value|
+ #         if value == min_value
+ #        	# return key
+ #            return_variable = key
+ #         end  		
+ #      }
+ #    return_variable
 
-         if value == min_value
-        	# return key
-            return_variable = key
-         end  		
-      }
-      
-    return_variable
+ ##################sorter#####################
+    min_value = nil
+    min_key = nil
+    name_hash.each{|key, value|
+       if min_value == nil or value < min_value
+         min_value = value
+         min_key = key 
+       end
+    }
+    min_key
 end
 
 # binding.pry
