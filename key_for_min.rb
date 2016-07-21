b09e3d009c77aa69175af5cadad391c3f9ee156b
abidiaz <abigaildiaz@gmail.com>
2016-07-20 23:50:57 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -20,25 +20,3 @@ def key_for_min_value(name_hash)
   end
   return lowest_key 
 end
-
-
-#hash = {:blake => 500, :ashley => 2, :adam => 1}
-#hash2 = {:blake => 10, :ashley => 50, :adam => 17}
-
-#lowest_value = 10
-#lowest_key = blake
-#value = 50
-#key = ashley
-
-
-#def key_for_min_value(hash)
-#  lowest_key = nil
-#  lowest_value = nil
-#  hash.each do |k, v|
-#    if lowest_value == nil || v < lowest_value
-#      lowest_value = v
-#      lowest_key = k
-#    end
-#  end
-#  lowest_key
-#end
