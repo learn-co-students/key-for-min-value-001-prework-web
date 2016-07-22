29447aa0b2fd275522560f6ddd9215073489a418
mo6709 <mo6709@gmail.com>
2016-07-22 16:32:12 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,22 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-    
-	# values = name_hash.collect{|key, value|
- #       value
-	# }
-	# min_value = values.min
-	# return_variable = nil
- #      name_hash.each{|key, value|
-
- #         if value == min_value
- #        	# return key
- #            return_variable = key
- #         end  		
- #      }
- #    return_variable
-
- ##################sorter#####################
     min_value = nil
     min_key = nil
     name_hash.each{|key, value|
@@ -28,5 +12,4 @@ def key_for_min_value(name_hash)
     }
     min_key
 end
-
 # binding.pry
