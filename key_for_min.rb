e802dc9c5b36ab272b4ef9ffb14400b51b5c707d
OliviaGunton <olivia.gunton@gmail.com>
2016-07-27 20:58:04 -0400

    Find key of smallest val in hash using only collect

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest_val_key = nil
+  smallest_val = nil
 
+  name_hash.collect do |key, val|
+    if smallest_val == nil || val < smallest_val
+      smallest_val_key = key
+      smallest_val = val
+    end
+  end
+
+  smallest_val_key
 end
