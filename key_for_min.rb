d2703971390d52ab04df71fbce4d8b6a513bd003
Jason Kwong <jasonkwong11@gmail.com>
2017-09-04 00:18:51 -0700

    refactor

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,33 +1,16 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  if name_hash != {}
- #collect the values and put them in an array
-  values_array = []
-  name_hash.collect do |key, value|
-    values_array.push(value)
-  end
-  #collect the keys and put them in an array
-  keys_array = []
-  name_hash.collect do |k,v|
-     keys_array.push(k)
-  end
-#figure out which value is the lowest and get the index of that value.... use that index with keys array and return that key
-  current_lowest_val = 0
-  banked_lowest_value = values_array[0]
-  values_array.each do |x|
-    current_lowest_val += x
-    if current_lowest_val <= banked_lowest_value
-      banked_lowest_value = x
-      current_lowest_val -= x
+def key_for_min_value(hash)
+  if hash.empty?
+    return nil
+  else
+    current_min = Float::INFINITY
+    min_key = ""
+    hash.each do |k, v|
+      if v < current_min
+        current_min = v
+        min_key = k
+      end
     end
-banked_lowest_value
+    min_key
   end
-
-  index = values_array.index(banked_lowest_value)
-  keys_array[index]
-else
-  nil
-end
 end
